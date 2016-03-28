47591e44e28ea1073d7074f3048927cae2be0803
HIYASHUHEI <syuhei@HIYASHUHEI-no-MacBook-Pro.local>
2016-03-28 23:04:09 +0900

    Updated enebularstorage

--- enebularstorage.js
+++ enebularstorage.js
@@ -14,124 +14,13 @@
  * limitations under the License.
  **/
 
-var mongo = require('mongodb');
+var request = require('request');
+var settings = require('./settings');
 var when = require('when');
 var util = require('util');
 
 var settings;
 
-var mongodb;
-var appname;
-
-//var heartBeatLastSent = (new Date()).getTime();
-//
-//setInterval(function () {
-//    var now = (new Date()).getTime();
-//    if (mongodb && now - heartBeatLastSent > 15000) {
-//        heartBeatLastSent = now;
-//        mongodb.command({ ping: 1}, function (err, result) {});
-//    }
-//}, 15000);
-
-function jconv(credentials) {
-    var jconvs = {};
-    for (id in credentials) {
-        jconvs[id.replace("_", ".")] = credentials[id];
-    }
-    return jconvs;
-}
-
-function bconv(credentials) {
-    var bconvs = {};
-    for (id in credentials) {
-        bconvs[id.replace(".", "_")] = credentials[id];
-    }
-    return bconvs;
-}
-
-function db() {
-    return when.promise(function(resolve,reject,notify) {
-        if (!mongodb) {
-            mongo.MongoClient.connect(settings.mongoUrl,
-                {
-                    db:{
-                        retryMiliSeconds:1000,
-                        numberOfRetries:3
-                    },
-                    server:{
-                        poolSize:1,
-                        auto_reconnect:true,
-                        socketOptions:{
-                            socketTimeoutMS:10000,
-                            keepAlive:1
-                        }
-                    }
-                },
-                function(err,_db) {
-                    if (err) {
-                        util.log("Mongo DB error:"+err);
-                        reject(err);
-                    } else {
-                        mongodb = _db;
-                        resolve(_db);
-                    }
-                }
-            );
-        } else {
-            resolve(mongodb);
-        }
-    });
-}
-
-function collection() {
-    return when.promise(function(resolve,reject,notify) {
-        db().then(function(db) {
-            db.collection(settings.mongoCollection||"nodered",function(err,_collection) {
-                if (err) {
-                    util.log("Mongo DB error:"+err);
-                    reject(err);
-                } else {
-                    resolve(_collection);
-                }
-            });
-        }).otherwise(function (err) {
-            reject(err);
-        })
-    });
-}
-
-function libCollection() {
-    return when.promise(function(resolve,reject,notify) {
-        db().then(function(db) {
-            db.collection(settings.mongoCollection||"nodered"+"-lib",function(err,_collection) {
-                if (err) {
-                    util.log("Mongo DB error:"+err);
-                    reject(err);
-                } else {
-                    resolve(_collection);
-                }
-            });
-        }).otherwise(function (err) {
-            reject(err);
-        })
-    });
-}
-
-function close() {
-    return when.promise(function(resolve,reject,notify) {
-        if (mongodb) {
-            mongodb.close(true, function(err,result) {
-                if (err) {
-                    util.log("Mongo DB error:"+err);
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            })
-            mongodb = null;
-        }
-    });
-}
 
 function timeoutWrap(func) {
     return when.promise(function(resolve,reject,notify) {
@@ -141,13 +30,9 @@ function timeoutWrap(func) {
             resolve(a,b,c,d);
         });
         promise.otherwise(function(err) {
-            console.log("TIMEOUT: ",func.name);
+            console.log("TIMEOUT: ",func);
             if (err == "timeout") {
-                close().then(function() {
-                    resolve(func());
-                }).otherwise(function(err) {
-                    reject(err);
-                });
+                resolve(func());
             } else {
                 reject(err);
             }
@@ -155,252 +40,157 @@ function timeoutWrap(func) {
     });
 }
 
-function getFlows() {
+function getEnebularFlow(cb) {
     var defer = when.defer();
-    collection().then(function(collection) {
-        collection.findOne({appname:appname},function(err,doc) {
-            if (err) {
-                defer.reject(err);
+    var url = settings.enebularUrl + "/flows/"+settings.flowId+"?access_token=" + settings.accessToken;
+    request.get(
+        {url: url, json:false},
+        function (err, res, body) {
+            if (!err && res.statusCode == 200) {
+                var data = JSON.parse(body);
+                defer.resolve( JSON.parse(cb(data)) );
             } else {
-                if (doc && doc.flow) {
-                    defer.resolve(doc.flow);
-                } else {
-                    defer.resolve([]);
-                }
-            }
-        })
-    }).otherwise(function(err) {
-        defer.reject(err);
-    });
+                defer.reject(err);
+            }   
+        }   
+    );
     return defer.promise;
+}
+
+function saveEnebularFlow(params) {
+    return new Promise(function(resolve, reject) {
+        var url = settings.enebularUrl + "/flows/"+settings.flowId+"?access_token=" + settings.accessToken;
+        request({ url: url, method: 'PUT', json: params, function(err, res, body) {
+                if (!err && res.statusCode == 200) {
+                    console.log("save flows to enebular");
+                    resolve();
+                } else {
+                    reject(err);
+                }   
+            }
+        });
+    });
+}
+
+function getFlows() {
+    return getEnebularFlow(function(data) {
+        return (data.body);
+    });
 }
 
 function saveFlows(flows) {
-    console.log("saveFlows");
-    var defer = when.defer();
-    collection().then(function(collection) {
-        collection.update({appname:appname},{$set:{appname:appname,flow:flows}},{upsert:true},function(err) {
-            if (err) {
-                defer.reject(err);
-            } else {
-                defer.resolve();
-            }
-        })
-    }).otherwise(function(err) {
-        defer.reject(err);
-    });
-    return defer.promise;
+    var params = {
+      "body": JSON.stringify(flows)
+    };
+    return saveEnebularFlow(params);
 }
 
 function getCredentials() {
-    var defer = when.defer();
-    collection().then(function(collection) {
-        collection.findOne({appname:appname},function(err,doc) {
-            if (err) {
-                defer.reject(err);
-            } else {
-                if (doc && doc.credentials) {
-                    defer.resolve(jconv(doc.credentials));
-                } else {
-                    defer.resolve({});
-                }
-            }
-        })
-    }).otherwise(function(err) {
-        defer.reject(err);
+    return getEnebularFlow(function(data) {
+        return (data.cred);
     });
-    return defer.promise;
 }
 
 function saveCredentials(credentials) {
-    var defer = when.defer();
-    collection().then(function(collection) {
-        collection.update({appname:appname},{$set: {"credentials":bconv(credentials)}},{upsert:true},function(err) {
-            if (err) {
-                defer.reject(err);
-            } else {
-                defer.resolve();
-            }
-        })
-    }).otherwise(function(err) {
-        defer.reject(err);
-    });
-    return defer.promise;
+    var params = {
+      "cred": JSON.stringify(credentials)
+    };
+    return saveEnebularFlow(params);
 }
 
 function getSettings () {
     var defer = when.defer();
-    collection().then(function(collection) {
-        collection.findOne({appname:appname},function(err,doc) {
-            if (err) {
-                defer.reject(err);
-            } else {
-                if (doc && doc.settings) {
-                    defer.resolve(jconv(doc.settings));
-                } else {
-                    defer.resolve({});
-                }
-            }
-        })
-    }).otherwise(function(err) {
-        defer.reject(err);
-    });
+    defer.resolve({});
     return defer.promise;
 }
 
 function saveSettings (settings) {
     var defer = when.defer();
-    collection().then(function(collection) {
-        collection.update({appname:appname},{$set: {"settings":bconv(settings)}},{upsert:true},function(err) {
-            if (err) {
-                console.log(err);
-                defer.reject(err);
-            } else {
-                defer.resolve();
-            }
-        })
-    }).otherwise(function(err) {
-        defer.reject(err);
-    });
+    defer.resolve();
     return defer.promise;
 }
 
 function getAllFlows() {
     var defer = when.defer();
-    libCollection().then(function(libCollection) {
-        libCollection.find({appname:appname, type:'flow'},{sort:'path'}).toArray(function(err,docs) {
-            if (err) {
-                defer.reject(err);
-            } else if (!docs) {
-                defer.resolve({});
+    var url = settings.enebularUrl + "/projects/"+settings.projectId+"/flows?access_token=" + settings.accessToken;
+    request.get(
+        {url: url, json:false},
+        function (err, res, body) {
+            if (!err && res.statusCode == 200) {
+                var flows = JSON.parse(body);
+                defer.resolve({f:flows.map(function(f) {
+                    return f.id;
+                })});
             } else {
-                var result = {};
-                for (var i=0;i<docs.length;i++) {
-                    var doc = docs[i];
-                    var path = doc.path;
-                    var parts = path.split("/");
-                    var ref = result;
-                    for (var j=0;j<parts.length-1;j++) {
-                        ref['d'] = ref['d']||{};
-                        ref['d'][parts[j]] = ref['d'][parts[j]]||{};
-                        ref = ref['d'][parts[j]];
-                    }
-                    ref['f'] = ref['f']||[];
-                    ref['f'].push(parts.slice(-1)[0]);
-                }
-                defer.resolve(result);
-            }
-        });
-    }).otherwise(function(err) {
-        defer.reject(err);
-    });
+                defer.reject(err);
+            }   
+        }   
+    );
     return defer.promise;
 }
 
 function getFlow(fn) {
     var defer = when.defer();
-    libCollection().then(function(libCollection) {
-        libCollection.findOne({appname:appname, type:'flow', path:fn},function(err,doc) {
-            if (err) {
-                defer.reject(err);
-            } else if (doc&& doc.data) {
-                defer.resolve(doc.data);
+    var url = settings.enebularUrl + "/flows/"+fn+"?access_token=" + settings.accessToken;
+    request.get(
+        {url: url, json:false},
+        function (err, res, body) {
+            if (!err && res.statusCode == 200) {
+                var enebularFlow = JSON.parse(body);
+                defer.resolve(JSON.parse(enebularFlow.body));
             } else {
-                defer.reject();
-            }
-        });
-    }).otherwise(function(err) {
-        defer.reject(err);
-    });
+                defer.reject(err);
+            }   
+        }   
+    );
     return defer.promise;
 }
 
 function saveFlow(fn,data) {
     var defer = when.defer();
-    libCollection().then(function(libCollection) {
-        libCollection.update({appname:appname,type:'flow',path:fn},{appname:appname,type:'flow',path:fn,data:data},{upsert:true},function(err) {
-            if (err) {
-                defer.reject(err);
-            } else {
+    var url = settings.enebularUrl + "/flows/"+fn+"?access_token=" + settings.accessToken;
+    var params = {
+      "title": fn,
+      "description": "",
+      "body": data,
+      "tags": [],
+      "userId": settings.userId
+    };
+    request({ url: url, method: 'PUT', json: params, function(err, res, body) {
+            if (!err && res.statusCode == 200) {
+                console.log("save flows to enebular");
                 defer.resolve();
-            }
-        });
-    }).otherwise(function(err) {
-        defer.reject(err);
+            } else {
+                defer.reject(err);
+            }   
+        }
     });
     return defer.promise;
 }
 
 function getLibraryEntry(type,path) {
+    console.log(type,path);
     var defer = when.defer();
-    libCollection().then(function(libCollection) {
-        libCollection.findOne({appname:appname, type:type, path:path}, function(err,doc) {
-            if (err) {
-                defer.reject(err);
-            } else if (doc) {
-                defer.resolve(doc.data);
-            } else {
-                if (path != "" && path.substr(-1) != "/") {
-                    path = path+"/";
-                }
-                libCollection.find({appname:appname, type:type, path:{$regex:path+".*"}},{sort:'path'}).toArray(function(err,docs) {
-                    if (err) {
-                        defer.reject(err);
-                    } else if (!docs) {
-                        defer.reject("not found");
-                    } else {
-                        var dirs = [];
-                        var files = [];
-                        for (var i=0;i<docs.length;i++) {
-                            var doc = docs[i];
-                            var subpath = doc.path.substr(path.length);
-                            var parts = subpath.split("/");
-                            if (parts.length == 1) {
-                                var meta = doc.meta;
-                                meta.fn = parts[0];
-                                files.push(meta);
-                            } else if (dirs.indexOf(parts[0]) == -1) {
-                                dirs.push(parts[0]);
-                            }
-                        }
-                        defer.resolve(dirs.concat(files));
-                    }
-                });
-            }
-        });
-    }).otherwise(function(err) {
-        defer.reject(err);
-    });
+    defer.resolve([]);
     return defer.promise;
 }
 
 function saveLibraryEntry(type,path,meta,body) {
     var defer = when.defer();
-    libCollection().then(function(libCollection) {
-        libCollection.update({appname:appname,type:type,path:path},{appname:appname,type:type,path:path,meta:meta,data:body},{upsert:true},function(err) {
-            if (err) {
-                defer.reject(err);
-            } else {
-                defer.resolve();
-            }
-        });
-    }).otherwise(function(err) {
-        defer.reject(err);
-    });
+    console.log(type,path,meta,body);
+    defer.resolve();
     return defer.promise;
 }
 
 var enebularstorage = {
     init: function(_settings) {
         settings = _settings;
-        appname = settings.mongoAppname || require('os').hostname();
-        return db();
     },
     getFlows: function() {
         return timeoutWrap(getFlows);
     },
     saveFlows: function(flows) {
-        return timeoutWrap(function(){return saveFlows(flows);});
+        saveFlows(flows);
     },
 
     getCredentials: function() {
--- settings.js
+++ settings.js
@@ -13,8 +13,13 @@ var settings = {
   }
 };
 
-if (process.env.MONGO_URI || process.env.MONGOLAB_URI) {
+if (process.env.ISSUER && process.env.USER_ID) {
   settings.storageModule = require('./enebularstorage');
+  settings.enebularUrl = process.env.ISSUER ? (process.env.ISSUER + '/api') : "http://localhost:7000/api";
+  settings.userId = process.env.USER_ID;
+  settings.projectId = process.env.PROJECT_ID;
+  settings.flowId = process.env.FLOW_ID;
+  settings.accessToken = process.env.ACCESS_TOKEN;
   settings.mongoUrl = process.env.MONGO_URI || process.env.MONGOLAB_URI;
   settings.mongoAppname = 'enebular';
 } else {
