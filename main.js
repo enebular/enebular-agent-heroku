d10fd31bf9c8cebae6ec19ab8b6218f853298e1f
HIYASHUHEI <syuhei@HIYASHUHEI-no-MacBook-Pro.local>
2016-03-30 10:31:35 +0900

    Added env.PORT

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ restart();
 function restart(_envs) {
 	var envs = _envs || {};
 	envs.ISSUER = process.env.ISSUER;
+	envs.PORT = process.env.PORT;
 	var app = child_process.fork("./app", [], {env:envs});
 	app.on("message", function (msg) {
 	    if(msg.event == 'restart') {
